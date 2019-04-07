dab0439f67759283363ff1f623abc5d8fb964d3e
harrylaw <harrylaw86@gmail.com>
2019-04-06 23:43:43 -0400

    Basic working personal diary developed with Ruby on Rails

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "harry", password: "harrylaw", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
